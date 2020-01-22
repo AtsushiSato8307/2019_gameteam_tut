4f35517a683704554fe312dda1b67fc84180e30b
Suguru820 <m011820708@edu.teu.ac.jp>
2020-01-22 17:37:06 +0900

    Merge branch 'enemy作成' of https://github.com/AtsushiSato8307/2019_gameteam_tut into Hasegawa
    # Conflicts:
    #	Assets/2.Sato.meta

---
+++ Assets/3.Uchiyama/Script/EnemyNav.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNav : MonoBehaviour
+{
+    public GameObject target;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = target.transform.position;
+    }
+}
