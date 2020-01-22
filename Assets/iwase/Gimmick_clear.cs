2af682148b9d2aff87619f8fa1e1df4303f046d5
atca <m011802608@edu.teu.ac.jp>
2020-01-22 18:39:33 +0900

    ギミック達成の反応を追加

---
+++ Assets/iwase/Gimmick_clear.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;using gimmick_durability;using UnityEngine.UI;
+
+public class Gimmick_clear : MonoBehaviour
+{  //ギミックが達成された時の反応（ギミックに付けるスクリプト）
+   
+    [SerializeField] Slider slider;
+    [SerializeField] Transform gimmick_slider;
+ 
+    //ギミックが達成されたかどうか
+    bool gimiick_status;
+
+    //耐久値の取得
+   // [SerializeField]
+ //  public float durability_now; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gimmick_slider = transform.Find("Gimmick_slider");
+        slider = gimmick_slider.transform.Find("Slider").GetComponent<Slider>();
+       // durability_now = transform.Find("durability_now");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (slider.value <= 0)
+        { //ギミックが達成された時の反応//現状は消すだけ
+            Destroy(this.gameObject);
+        }
+
+    }
+}
--- Assets/iwase/Gimmick_durability.cs
+++ Assets/iwase/Gimmick_durability.cs
@@ -3,39 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
-public class Gimmick_durability : MonoBehaviour
+namespace gimmick_durability
 {
-    [SerializeField] float maxdurability = 500f;
-    [SerializeField] float durability_now = 500f;
-    [SerializeField] Slider slider;
-    [SerializeField] Transform gimmick_slider;
+    public class Gimmick_durability : MonoBehaviour
+    {
+        [SerializeField] float maxdurability = 500f;
+        [SerializeField] float durability_now = 500f;
+        [SerializeField] Slider slider;
+        [SerializeField] Transform gimmick_slider;
 
-    //ギミックへのアクセス（ダメージ）判定 //現状の判定条件："//当たり判定" 
-    [SerializeField] bool gimmick_hanntei = false;
+        //ギミックへのアクセス（ダメージ）判定 //現状の判定条件："//当たり判定" 
+        [SerializeField] bool gimmick_hanntei = false;
 
-    //当たり判定　    
-    void OnCollisionEnter(Collision collision)
-    //tagで管理する場合 
-    {  if (collision.gameObject.tag == "soldier")
-　     { gimmick_hanntei = true; }}
-    void OnCollisionExit(Collision collision) { gimmick_hanntei = false; }
+        //当たり判定　    
+        void OnCollisionEnter(Collision collision)
+        //tagで管理する場合 
+        {
+            if (collision.gameObject.tag == "soldier")
+            { gimmick_hanntei = true; }
+        }
+        void OnCollisionExit(Collision collision)
+        {
+            gimmick_hanntei = false;
+        }
+        // float Mathf.Clamp(durability_now,0, maxdurability)
 
+        // Start is called before the first frame update
+        void Start()
+        {
+            gimmick_slider = transform.Find("Gimmick_slider");
+            slider = gimmick_slider.transform.Find("Slider").GetComponent<Slider>();
+            maxdurability = slider.maxValue;
 
-    // Start is called before the first frame update
-    void Start()
-     {
-        // GameObject gimmick = GameObject.Find("gimmick_kari"); 
-        gimmick_slider = transform.Find("Gimmick_slider");
-        slider = gimmick_slider.transform.Find("Slider").GetComponent<Slider>();
-        maxdurability = slider.maxValue;
-      }   
+        }
 
-    // Update is called once per frame
-    void Update()
-     {
-        //ギミックにアクセスした際の処理　
-       if (gimmick_hanntei == true) { durability_now--; Debug.Log("残り＝" + (durability_now)); }
-        slider.value = durability_now;
-     }
+        // Update is called once per frame
+        void Update()
+        {
+            //ギミックにアクセスした際の処理　
+            if (gimmick_hanntei == true) { durability_now--; Debug.Log("残り＝" + (durability_now)); }
+            slider.value = durability_now;
+        }
+    }
 }
